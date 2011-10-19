665ce51907149a3a98140977f8c415ec0ce1b362
Zoran Simic <zoran@simicweb.com>
2011-10-19 00:20:31 -0700

    First commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+java -ea -cp lib/ants-api.jar:lib/ants-server.jar:bin/build/libs/ants-impl.jar org/linkedin/contest/ants/server/AntServer -B -p1 org.linkedin.contest.ants.impl.ZoranAnt -p2 org.linkedin.contest.ants.impl.ZoranAnt
+
+#main class: org/linkedin/contest/ants/server/AntServer
+#args: -B -p1 org.linkedin.contest.ants.impl.ZoranAnt -p2 org.linkedin.contest.ants.impl.DoNothingAnt
