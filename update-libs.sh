177d3606599e06402721d38cd9920f6612e890bb
Zoran Simic <zsimic@linkedin.com>
2011-10-24 12:12:40 -0700

    Added jar update helper script

---
+++ update-libs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd lib
+rm ants-api.jar
+rm ants-server.jar
+wget -q https://iwww.corp.linkedin.com/wiki/cf/download/attachments/41116114/ants-api.jar
+wget -q https://iwww.corp.linkedin.com/wiki/cf/download/attachments/41116114/ants-server.jar
