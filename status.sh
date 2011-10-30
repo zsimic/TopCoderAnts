11375b578db0525b58f49260016987dc6e25853d
Zoran Simic <zoran@simicweb.com>
2011-10-29 23:59:00 -0700

    Minor

--- status.sh
+++ status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find logs -depth 1 -exec grep -H -n -i avoiding {} \;
-find logs -depth 1 -exec grep -H -n -i died {} \;
+#find logs -depth 1 -exec grep -H -n -i avoiding {} \;
+#find logs -depth 1 -exec grep -H -n -i died {} \;
 cat logs/trace_*.txt | grep -c "drops food to 512,512"
