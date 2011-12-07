c08a92cbcb726222f71e3d46798ffea4e7116173
Zoran Simic <zoran@simicweb.com>
2011-12-07 09:29:50 -0800

    Minor

--- viewer/AntsViewer.py
+++ viewer/AntsViewer.py
@@ -339,7 +339,7 @@ class Toolbar(QtGui.QWidget):
     self.gr.sep1 = Gui.new_Label(self.gr.hbox, '', 12)
     self.gr.play = Gui.new_PushButton(self.gr.hbox, "Play", self.on_play_pause, 1)
     self.gr.speed_label = Gui.new_Label(self.gr.hbox, 'Speed:', 1)
-    self.gr.speed = Gui.new_Combo(self.gr.hbox, ['256x', '128x', '64x', '32x', '16x', '8x', '4x', '2x', '1x'], self.on_speed_changed, 1)
+    self.gr.speed = Gui.new_Combo(self.gr.hbox, ['512x', '256x', '128x', '64x', '32x', '16x', '8x', '4x', '2x', '1x'], self.on_speed_changed, 1)
     self.gr.step = Gui.new_PushButton(self.gr.hbox, "Step", self.on_step, 1)
     self.gr.progress_bar = QtGui.QProgressBar()
     Gui.embox_and_size(self.gr.hbox, self.gr.progress_bar, None, 100)
@@ -436,7 +436,7 @@ class Toolbar(QtGui.QWidget):
     self.activate_play_buttons()
 
   def on_speed_changed(self, item):
-    self.speed_factor = SPEED_NORMAL_INTERVAL / (2 ** (8-item))
+    self.speed_factor = SPEED_NORMAL_INTERVAL / (2 ** (9-item))
     self.timer.setInterval(self.speed_factor)
     if len(self.games):
       self.estimate_eta()
